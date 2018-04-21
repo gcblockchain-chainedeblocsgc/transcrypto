9548e72364e7d6237c06571bef9f79f19ed8b658
Mariya Morosovska <masha@Mariyas-MacBook-Pro.local>
2018-04-21 15:57:40 -0400

    html base added

---
+++ transcrypto-frontend/js/app.js
@@ -0,0 +1,8 @@
+$(document).foundation()
+$('[data-addCart]').click(function() {
+  $(this).addClass('is-adding')
+  setTimeout(function() {
+  $('[data-addCart]').removeClass('is-adding')
+  $('[data-successMessage]').removeClass('hide')
+  }, 2500);
+});
