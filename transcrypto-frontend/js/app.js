7680be490e7ee97f67215713d045efc289883517
lgainsbrook <lauren.gainsbrook@gmail.com>
2018-04-22 21:45:20 -0400

    modify request button function - display message once

--- transcrypto-frontend/js/app.js
+++ transcrypto-frontend/js/app.js
@@ -1,8 +1,28 @@
-$(document).foundation()
-$('[data-addCart]').click(function() {
-  $(this).addClass('is-adding')
-  setTimeout(function() {
-  $('[data-addCart]').removeClass('is-adding')
-  $('[data-successMessage]').removeClass('hide')
-  }, 2500);
-});
+// $(document).foundation()
+// $('[data-addCart]').click(function() {
+//   $(this).addClass('is-adding')
+//   setTimeout(function() {
+//   $('[data-addCart]').removeClass('is-adding')
+//   $('[data-successMessage]').removeClass('hide')
+//   }, 2500);
+// });
+
+function requestSubmitted(e) {
+    let button = e.target;
+    let containerDiv = button.parentElement;
+    let message = containerDiv.children[1];
+    // button.classList.add('is-adding');
+    setTimeout(function() {
+        // button.classList.remove('is-adding');
+        message.classList.remove('hide');
+    }, 2500);
+}
+
+buttons = document.getElementsByClassName('button-add-cart');
+console.log(buttons[0].className + buttons [1] + buttons[2]);
+
+for (i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(e) {
+        requestSubmitted(e)}
+    );
+}
