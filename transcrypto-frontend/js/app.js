4bdedbb369406f087a75bafa01643b940454add6
lgainsbrook <lauren.gainsbrook@gmail.com>
2018-04-22 21:52:08 -0400

    refactor code

--- transcrypto-frontend/js/app.js
+++ transcrypto-frontend/js/app.js
@@ -1,12 +1,17 @@
+///// ORIGINAL jQUERY CODE /////
+//
 // $(document).foundation()
 // $('[data-addCart]').click(function() {
 //   $(this).addClass('is-adding')
+//   console.log(this);
 //   setTimeout(function() {
 //   $('[data-addCart]').removeClass('is-adding')
 //   $('[data-successMessage]').removeClass('hide')
 //   }, 2500);
 // });
 
+
+///// LAUREN'S CODE /////
 function requestSubmitted(e) {
     let button = e.target;
     let containerDiv = button.parentElement;
@@ -18,11 +23,13 @@ function requestSubmitted(e) {
     }, 2500);
 }
 
-buttons = document.getElementsByClassName('button-add-cart');
-console.log(buttons[0].className + buttons [1] + buttons[2]);
-
-for (i=0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function(e) {
-        requestSubmitted(e)}
-    );
+function setEventListeners() {
+    buttons = document.getElementsByClassName('button-add-cart');
+    for (i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(e) {
+            requestSubmitted(e)}
+        );
+    }
 }
+
+setEventListeners();
